01535edabbdd88ef5b92ffc7f4393385862ba30c
Taster98 <luigi.gesuele98@gmail.com>
2021-04-18 22:22:26 +0200

    Added MainMenuActivity

---
+++ TrisGame/app/src/main/java/it/luiggi/projects/trisgame/MainMenuActivity.java
@@ -0,0 +1,27 @@
+package it.luiggi.projects.trisgame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class MainMenuActivity extends Activity implements View.OnClickListener {
+    private Button playButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+        playButton = (Button)findViewById(R.id.giocaButton);
+        playButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent myIntent = new Intent(MainMenuActivity.this, MainActivity.class);
+        this.startActivity(myIntent);
+    }
+}
